54ada6bdb3e0b98993d502afbaeafee8c0ba5c63
Ryan Wendling <wendlingryan@gmail.com>
2017-09-11 16:55:00 +0000

    added profile and  user associations

--- db/migrate/20170911162809_create_profiles.rb
+++ db/migrate/20170911162809_create_profiles.rb
@@ -7,6 +7,8 @@ class CreateProfiles < ActiveRecord::Migration[5.0]
       t.string :job_title
       t.string :phone_number
       t.string :contact_email
+      t.text :description
+      t.timestamps
     end
   end
 end
