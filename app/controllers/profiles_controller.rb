9168f7961919ad410f98671473cc78af3380386c
Ryan Wendling <wendlingryan@gmail.com>
2017-09-11 18:35:45 +0000

    Added profiles controller, new view, and link on homepage

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,6 @@
+class ProfilesController < ApplicationController
+    #When the user makes a GET request to /users/:user_id/profile/new, we want to run the 'new' action
+    def new
+        #user will want to see a blank profile details screen.
+    end
+end
