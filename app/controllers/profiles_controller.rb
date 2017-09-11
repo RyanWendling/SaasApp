e12be5e396349f2efdc5272414e1684227b23019
Ryan Wendling <wendlingryan@gmail.com>
2017-09-11 19:44:49 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,5 +2,6 @@ class ProfilesController < ApplicationController
     #When the user makes a GET request to /users/:user_id/profile/new, we want to run the 'new' action
     def new
         #user will want to see a blank profile details screen.
+        @profile = Profile.new
     end
 end
