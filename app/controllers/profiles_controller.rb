e7cc44c5ab4dba66f9664892fa54bc302546bec0
Ryan Wendling <wendlingryan@gmail.com>
2017-09-12 23:23:42 +0000

    added  profile edit action and view

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -18,6 +18,13 @@ class ProfilesController < ApplicationController
         render action: :new
       end
     end
+    
+    # This is for get requests made to users/:uer_id/profile/edit
+    def edit
+      @user = User.find(params[:user_id])
+      @profile = @user.profile
+    end
+    
     private
       def profile_params
         params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
